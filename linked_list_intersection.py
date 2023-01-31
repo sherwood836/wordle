f76798183ab1befbe54abedfc3a07708d7481ca2
Tommy Di <tdisessa@gmail.com>
2023-01-31 10:54:43 -0600

    Added a test for looped linked lists and huge list test

--- linked_list_intersection.py
+++ linked_list_intersection.py
@@ -10,30 +10,40 @@ def find_intersect(list_input, search_value):
     node_set = set()
     found_value = False
 
-    for head in head_list:
+    for head in head_list:  # line 13
         curr = head
+        traverse_node_set = set()
 
-        while curr:
-            if curr in node_set:
-                found_value = True
-            
-            node_set.add(curr)
-            curr = list_dict.get(curr)
+        if not found_value:
+            while curr and curr not in traverse_node_set:   # line 16
+                if curr in node_set:  # line 17
+                    found_value = True
+                    break
+                
+                node_set.add(curr)
+                traverse_node_set.add(curr)
+                curr = list_dict.get(curr)
 
     return found_value
 
 
 import unittest
+import timeit
+import string
+
+input = '\n'.join([f'{char[0]}->{char[1]}' for char in zip(string.ascii_lowercase[:-1], string.ascii_lowercase[1:])]) + "\nz->d"
 
 class FloydTest(unittest.TestCase):
     def test_find_intersect(self):
-        input = '''a->b
-b->c
-z->x
-x->y
-w->c
-'''
-        self.assertFalse(find_intersect(input, "a,x"))
+
+        print(timeit.timeit('find_intersect(input, "a,a")', setup="from __main__ import find_intersect, input",  number=10000))
+        print(timeit.timeit('find_intersect(input, "a,m")', setup="from __main__ import find_intersect, input",  number=10000))
+        print(timeit.timeit('find_intersect(input, "a,x")', setup="from __main__ import find_intersect, input",  number=10000))
+
+        input = '\n'.join([f'{char[0]}->{char[1]}' for char in zip(string.ascii_lowercase[:-1], string.ascii_lowercase[1:])])
+        input += "\nz->d"
+        print(input)
+        self.assertTrue(find_intersect(input, "a,x"))
         self.assertTrue(find_intersect(input, "a,w"))
 
 
